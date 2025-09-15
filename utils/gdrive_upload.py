dd523e028e7d00ce2ed4f590b7b41609cad51594
Neharika <neharika@APPL-LAP-114.local>
2025-09-15 15:55:59 +0530

    Updated gdrive_upload.py

--- utils/gdrive_upload.py
+++ utils/gdrive_upload.py
@@ -1,31 +1,47 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import os
-from dotenv import load_dotenv
-load_dotenv()  # this loads variables from .env into os.environ
 
 FOLDER_ID = "138q20HHJFDxrQ87t5fVf-nOBYqc5jJxf"
-gauth = GoogleAuth()
-cred_path = os.getenv("GOOGLE_DRIVE_CREDENTIALS", "/etc/secrets/credentials.json")
-gauth.LoadCredentialsFile(cred_path)
 
-if gauth.credentials is None:
-    # first-time only, locally run this to generate credentials.json
-    gauth.LocalWebserverAuth()
-    gauth.SaveCredentialsFile("credentials.json")
-elif gauth.access_token_expired:
-    gauth.Refresh()
-else:
-    gauth.Authorize()
+def authenticate_drive():
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile("credentials.json")
+    
+    if gauth.credentials is None:
+        raise Exception("No valid credentials found. Run auth flow locally first.")
+    elif gauth.access_token_expired:
+        gauth.Refresh()
+    else:
+        gauth.Authorize()
 
+    return GoogleDrive(gauth)
 
-drive = GoogleDrive(gauth)
-
-def upload_to_drive(file_path, filename, folder_id):
+def upload_to_drive(file_path, filename, folder_id=FOLDER_ID):
+    drive = authenticate_drive()
     file = drive.CreateFile({
         "title": filename,
-        "parents": [{"id": "138q20HHJFDxrQ87t5fVf-nOBYqc5jJxf"}]  # folder ID of your shared folder
+        "parents": [{"id": folder_id}]
     })
     file.SetContentFile(file_path)
     file.Upload()
     return f"https://drive.google.com/uc?export=view&id={file['id']}"
+
+
+
+# from pydrive.auth import GoogleAuth
+# from pydrive.drive import GoogleDrive
+
+# FOLDER_ID = "138q20HHJFDxrQ87t5fVf-nOBYqc5jJxf"
+# gauth = GoogleAuth()
+# gauth.LocalWebserverAuth()  # Opens browser once for login
+
+# drive = GoogleDrive(gauth)
+
+# def upload_to_drive(file_path, filename, folder_id):
+#     file = drive.CreateFile({
+#         "title": filename,
+#         "parents": [{"id": "138q20HHJFDxrQ87t5fVf-nOBYqc5jJxf"}]  # folder ID of your shared folder
+#     })
+#     file.SetContentFile(file_path)
+#     file.Upload()
+#     return f"https://drive.google.com/uc?export=view&id={file['id']}"
